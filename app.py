09f5e32a2dc8d8f39387f6af833470ac86193df8
manticarodrigo <manticarodrigo@gmail.com>
2018-09-30 21:55:33 -0400

    changed schema for week accessibility

--- app.py
+++ app.py
@@ -1,22 +1,22 @@
 from sheets import getSheet
 
 def main():
-    values = getSheet('1BNJKDTg_-c84h5BpSOkJTQOL4axuxCPB-rLaCbHAcfg', 'db!A2:J18')
+    values = getSheet('1BNJKDTg_-c84h5BpSOkJTQOL4axuxCPB-rLaCbHAcfg', 'db!A2:J500000')
     sku_dict = {}
     for row in values:
         sku = int(row[0])
-        unit_sale = int(row[5])
+        unit_sale = float(row[5])
         week = int(row[8])
         store = row[9]
         if sku not in sku_dict:
             sku_dict.setdefault(sku, {})
-        if store not in sku_dict[sku]:
-            sku_dict[sku].setdefault(store, {})
-        if week not in sku_dict[sku][store]:
-            sku_dict[sku][store].setdefault(week, unit_sale)
+        if week not in sku_dict[sku]:
+            sku_dict[sku].setdefault(week, {})
+        if store not in sku_dict[sku][week]:
+            sku_dict[sku][week].setdefault(store, unit_sale)
 
-        accumulator = sku_dict[sku][store][week]
-        sku_dict[sku][store][week] = (accumulator + unit_sale) / 2
+        accumulator = sku_dict[sku][week][store]
+        sku_dict[sku][week][store] = (accumulator + unit_sale) / 2
         
     print(sku_dict)
 
